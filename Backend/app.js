d92c65441e6497e0b482901ea8b574614ada5e07
Kiran C. Kankariya <kirankankariya097+git@gmail.com>
2023-12-06 15:13:57 +0530

    try again\

--- Backend/app.js
+++ Backend/app.js
@@ -18,9 +18,9 @@ const port = process.env.PORT
 
 if (process.env.NODE_ENV === "production") {
     const path = require("path");
-    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+    app.use(express.static(path.resolve(__dirname, 'Frontend', 'build')));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'), function (err) {
+        res.sendFile(path.resolve(__dirname, 'Frontend', 'build', 'index.html'), function (err) {
             if (err) {
                 res.status(500).send(err)
             }
